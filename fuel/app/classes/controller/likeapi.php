3dbabcd6feff46eea44bedc8bcc1e3c7579e2e0b
do-hoc-mulodo <do.hoc@mulodo.com>
2014-04-24 17:22:04 +0100

    Controller API v0.1.0 20140424

---
+++ fuel/app/classes/controller/likeapi.php
@@ -0,0 +1,26 @@
+<?php 
+class Controller_Likeapi extends Controller_Rest
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     PHP-Training Team
+ * @license    MIT License
+ * @copyright  do.hoc@mulodo.com
+ * @link       http://fuelphp.com
+ */
+ 
+/**
+ * The LikeAPI Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+{
+	
+}
+?>
