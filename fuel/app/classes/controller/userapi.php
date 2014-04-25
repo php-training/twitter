d7e578a19969dbdc3ff975b1ff335e85b0ad1ba1
Tran Quang Hoa <hoaqt@Hoats-iMac.local>
2014-04-25 11:49:48 +0700

    new version

--- fuel/app/classes/controller/userapi.php
+++ fuel/app/classes/controller/userapi.php
@@ -15,18 +15,41 @@
 		/**
 		 * ERROR CODE
 		 */
-		protected $STATUS_OK         = 200;
-		protected $STATUS_DB_ERR     = 500;
-		protected $STATUS_VALID_ERR  = 401;
-		protected $STATUS_EXST_ACC   = 402;
-		protected $EXPIRE_PERIOD     = 172800; //2 days
+		/*
+		private $ERROR = array (
+		"OK"            => array (200, ""), //succeeded
+		"DB_ERR"        => array (500, "DATABASE ERROR"), //database error
+		"INVALID_ERR"   => array (401, "INVALID ARGUMENT"), //invalid argument
+		"EXST_ACC_ERR"  => array (402, "EXISTED ACCOUNT"), //account is existed
+		"NEXST_ACC_ERR" => array (402, "EMAIL OR PASSWORD IS NOT CORRECT"), //not existed
+		);
+		*/
+		/**
+		 * STATUS CODE
+		 */
+		const STATUS_OK               = 200; //succeeded
+		const STATUS_DB_ERR           = 500; //database error
+		const STATUS_INVALID_ERR      = 401; //invalid argument
+		const STATUS_EXST_ACC_ERR     = 402; //acc is existed
+		const STATUS_NEXST_ACC_ERR    = 402; //account (email & pass) is not existed.
+		const STATUS_NEXST_TOKEN_ERR  = 402; //token is not existed. (expired)
+		const EXPIRE_PERIOD           = 172800; //2 days
+		
+		/**
+		 * ERROR MESSAGE 
+		 */
+		const MSG_OK               = ""; //succeeded
+		const MSG_DB_ERR           = "DATABASE ERROR"; //database error
+		const MSG_INVALID_ERR      = "INVALID ARGUMENT"; //invalid argument
+		const MSG_EXST_ACC_ERR     = "EXISTED ACCOUNT"; //acc is existed
+		const MSG_NEXST_ACC_ERR    = "NOT EXISTED ACCOUNT"; //account (email & pass) is not existed.
+		const MSG_NEXST_TOKEN_ERR  = "NOT EXISTED TOKEN"; //token is not existed. (expired)
 		
 		/**
 		 * the index action: for testing only
 		 */
 		public function action_index(){
-			echo "Hi! I'm the user API of iSociety. :)";
-			
+			echo "Hi! I'm the user API of iSociety1. :)";
 		}
 		
 		/**
@@ -36,7 +59,8 @@
 		 */
 		public function action_login(){
 			
-			$error_code = $this -> STATUS_OK; //default OK
+			$error_code = self :: STATUS_OK; //default OK
+			$msg        = self :: MSG_OK;
 			
 			$token=""; //null
 				
@@ -46,7 +70,8 @@
 			$val -> add_field('password','Password','required|min_length[3]|max_length[10]');
 			
 			if (!$val -> run()){ //failed, VALID_ERR
-				$error_code = $this -> STATUS_VALID_ERR;
+				$error_code = self :: STATUS_INVALID_ERR;
+				$msg        = self :: MSG_INVALID_ERR;
 			}				
 			else{ //valid parameters, thus get the parameters and continue 
 					
@@ -61,7 +86,8 @@
 					));
 						
 					if (count($entry) == 0) { //login failed
-						$error_code = $this -> STATUS_EXST_ACC;
+						$error_code = self :: STATUS_NEXST_ACC_ERR;
+						$msg        = self :: MSG_NEXST_ACC_ERR;
 							
 					}
 					else{
@@ -72,20 +98,21 @@
 							$dbtoken   = new Model_Login_Token();	//if no error is found, insert token into login_token table
 							$dbtoken -> iduser   = $user -> id; //id user
 							$dbtoken -> token    = $token;
-							$dbtoken -> due_date = date('Y-m-d h:i:s' ,time() + $this->EXPIRE_PERIOD); //2 days
+							$dbtoken -> due_date = date('Y-m-d H:i:s' ,time() + self :: EXPIRE_PERIOD); //2 days
 							$dbtoken -> save();
 						}
 					}
 				
 				}
 				catch (Database_Exception $dbe){
-					$error_code = $this -> STATUS_DB_ERR;
+					$error_code = self :: STATUS_DB_ERR;
+					$msg        = self :: MSG_DB_ERR;
 				}
 
 			}
 			
 			
-			$result = array("error" => array("status" => $error_code,"message" => ""),"token" => $token); //print result in XML format
+			$result = array("error" => array("status" => $error_code,"message" => $msg),"token" => $token); //print result in XML format
 			print_r(Format :: forge($result) -> to_xml());
 
 		}
@@ -110,7 +137,8 @@
 			//echo "Hi! I'm signing up an account for you.";
 		
 
-			$error_code = $this -> STATUS_OK; 
+			$error_code = self :: STATUS_OK;
+			$msg        = self :: MSG_OK; 
 
 			$val = Validation::forge('signup_alidation'); //check the validation, if not set VALID_ERR
 			
@@ -118,7 +146,8 @@
 			$val -> add_field('password','Password','required|min_length[3]|max_length[10]');
 			
 			if (!$val->run()){
-				$error_code = $this -> STATUS_VALID_ERR; // invalid parameter
+				$error_code = self :: STATUS_INVALID_ERR; // invalid parameter
+				$msg        = self :: MSG_INVALID_ERR;
 			}
 			
 			else{ //ok
@@ -127,35 +156,36 @@
 				$pwd      = md5(Input :: post('password')); 	//get the post variable first
 						
 				try{
-					
-					if (count(Model_User :: find('all', array(
+					$numuser=count(Model_User :: find('all', array(
 							'where' => array(
 									array('email',$email))
-					)))>0) //validate paramenter 401
-								$error_code = $this -> STATUS_EXST_ACC;
-				
+					)));
+					if ($numuser>0) //validate paramenter, 401
+					{
+							$error_code = self :: STATUS_EXST_ACC_ERR;
+							$msg        = self :: MSG_EXST_ACC_ERR;
+					}
 							//then create a user model to insert into database
-							else{
+					else{
 									
-								$newuser             = new Model_User();
-								//sync data
-								$newuser -> email    = 	$email;
-								$newuser -> password = 	$pwd;
-								$newuser -> created_at = time();
-								//insert into databaseateime
-								$newuser -> save();
+							$newuser             = new Model_User();
+							//sync data
+							$newuser -> email    = 	$email;
+							$newuser -> password = 	$pwd;
+							$newuser -> created_at = time();
+							//insert into databaseateime
+							$newuser -> save();
 									
-							}
+					}
 				}
 				
 				catch (Database_Exception $dbe){
 					
-					$error_code = $this -> STATUS_DB_ERR;
+					$error_code = self :: STATUS_DB_ERR;
+					$msg        = self :: MSG_DB_ERR;
 				}	
 			}
-			
-			
-			$result = array("error" => array("status" => $error_code,"message" => "")); //print result in XML format
+			$result = array("error" => array("status" => $error_code,"message" => $msg)); //print result in XML format
 			print_r(Format :: forge($result) -> to_xml());
 			
 		}
@@ -168,12 +198,14 @@
 		public function action_logout(){
 			//echo "Hi! I'm loging out.";
 			
-			$error_code = $this -> STATUS_OK;
+			$error_code = self :: STATUS_OK;
+			$msg        = self :: MSG_OK;
 
 			$val = Validation :: forge('logout_validation'); //validation			
 			$val -> add_field ('token','Login_token','required|min_length[5]');
 			if (!$val -> run()){ //failed
-				$error_code = $this -> STATUS_VALID_ERR;
+				$error_code = self :: STATUS_INVALID_ERR;
+				$msg        = self :: MSG_INVALID_ERR;
 			}
 			else{ // validation pass, go the next step
 				$token = Input :: post ('token');
@@ -182,28 +214,30 @@
 					$entry = Model_Login_Token ::find('all', array (
 											'where' => array (//search the database for token 
 											array ('token', $token),
-											array ('due_date', '>=', date ('Y-m-d h:i:s', time())),
+											array ('due_date', '>=', date ('Y-m-d H:i:s', time())),
 											)
 											));
 					
 					
 					if (!count($entry)>0) { //token is not existed
-						$error_code = $this -> STATUS_EXST_ACC;
+						$error_code = self :: STATUS_NEXST_TOKEN_ERR;
+						$msg        = self :: MSG_NEXST_TOKEN_ERR;
 					}
 					else { //continue to expire the token
 					
 						foreach ($entry as $dbtoken){ //only one
-							$dbtoken -> due_date = date ('Y-m-d h:i:s', time());
+							$dbtoken -> due_date = date ('Y-m-d H:i:s', time());
 							$dbtoken -> save(); //updated
 						}
 					}
 				}
 				catch (Database_Exception $dbe){
-					$error_code = $this -> STATUS_DB_ERR;
+					$error_code = self :: STATUS_DB_ERR;
+					$msg        = self :: MSG_DB_ERR;
 				}
-			}
+			} 
 			
-			$result = array ("error" => array ("status" => $error_code, "message" => ""));
+			$result = array ("error" => array ("status" => $error_code, "message" => $msg));
 			print_r(Format :: forge ($result) -> to_xml()); //print the result in xml
 		}
 		
---
+++ fuel/app/logs/2014/04/25.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2014-04-25 11:33:32 --> Error - invalid data source name in /Users/hoaqt/Documents/iTwitter/twitter/fuel/core/classes/database/pdo/connection.php on line 100
+ERROR - 2014-04-25 11:35:21 --> Error - invalid data source name in /Users/hoaqt/Documents/iTwitter/twitter/fuel/core/classes/database/pdo/connection.php on line 100
+ERROR - 2014-04-25 11:38:49 --> Error - invalid data source name in /Users/hoaqt/Documents/iTwitter/twitter/fuel/core/classes/database/pdo/connection.php on line 100
+ERROR - 2014-04-25 11:41:56 --> Error - SQLSTATE[3D000]: Invalid catalog name: 1046 No database selected with query: "SELECT `t0`.`id` AS `t0_c0`, `t0`.`email` AS `t0_c1`, `t0`.`password` AS `t0_c2`, `t0`.`created_at` AS `t0_c3` FROM `users` AS `t0` WHERE `t0`.`email` = 'tran.hoa@mulodo.com'" in /Users/hoaqt/Documents/iTwitter/twitter/fuel/core/classes/database/pdo/connection.php on line 234
+ERROR - 2014-04-25 11:44:52 --> Notice - Undefined property: Controller_UserAPI::$EXPIRE_PERIOD in /Users/hoaqt/Documents/iTwitter/twitter/fuel/app/classes/controller/userapi.php on line 101
