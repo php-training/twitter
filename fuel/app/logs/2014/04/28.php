92194f5b09547ef82a3c93c97001d71cd85e73db
phung-van-mulodo <phung.van@mulodo.com>
2014-04-28 15:26:13 +0700

    update post

--- fuel/app/logs/2014/04/28.php
+++ fuel/app/logs/2014/04/28.php
@@ -90,3 +90,5 @@ WARNING - 2014-04-28 15:37:17 --> Fuel\Core\Fuel::init - The configured locale e
 WARNING - 2014-04-28 15:38:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 WARNING - 2014-04-28 15:38:19 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 >>>>>>> FETCH_HEAD
+ERROR - 2014-04-28 15:13:53 --> Warning - move_uploaded_file(upload/nhung-gi-da-qua-em-se-de-danh-suot-doi.jpg): failed to open stream: No such file or directory in /Users/van.phung267/htdocs/aaa/twitter/fuel/app/classes/controller/post.php on line 284
+ERROR - 2014-04-28 15:15:00 --> Warning - move_uploaded_file(upload/nhung-gi-da-qua-em-se-de-danh-suot-doi.jpg): failed to open stream: No such file or directory in /Users/van.phung267/htdocs/aaa/twitter/fuel/app/classes/controller/post.php on line 284
