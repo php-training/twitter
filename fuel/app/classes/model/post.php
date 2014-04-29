ca5acfa62bc38648274772e6a6307a83b1039203
phung-van-mulodo <phung.van@mulodo.com>
2014-04-29 17:00:39 +0700

    update post model

--- fuel/app/classes/model/post.php
+++ fuel/app/classes/model/post.php
@@ -13,6 +13,14 @@ class Post extends \Model
 		
 		return $result;
 	}
+        public static function checkPost($id_post='')
+	{
+		//$sql = DB::select('*')-> from('login_token')-> where('token', $token)-> and_where('due_date', '>=', 'NOW()');
+		$sql = DB::query("Select `id_user` from `post` where id = '" . $id_post . "' AND delete_status= 0");
+		$result = $sql->execute()->current();
+		
+		return $result;
+	}
 	
 	public static function insert($data)
 	{
@@ -27,6 +35,41 @@ class Post extends \Model
 			return false;
 		}	
 	}
+        public static function update($data){
+            
+            try
+		{
+			//$query = DB::update('post')->columns(array('delete_status'));
+                        $query = DB::query("update post set delete_status = 1"  . " where id = " . $data['id']);
+			$query->execute();
+			
+			return true;
+		} catch(\Database_Exception $e) {
+			return false;
+		}	
+        }
+        public static function update2($data){
+            
+            try
+		{
+                        $sql = "update post set content = '" . $data['content'] . "', image_url = '" . $data['image_url'] . "' where id='" . $data['id'] . "'";
+			$query = DB::query($sql);	
+                        //$query = DB::query("update follow set status = " . $status . " where user_id_follow = " . $user_id_follow . " and user_id_followed = " . $user_id_followed,DB::UPDATE);	
+                        $query->execute();
+			return true;
+		} catch(\Database_Exception $e) {
+			return false;
+		}	
+        }
+        public static function selectPost($id_user){
+            
+                $sql = DB::query("Select * from `post` where id_user = " . $id_user . " AND delete_status= 0");
+              //  $sql = DB::query("Select `iduser` from `login_token` where token = '" . $token . "' AND due_date >= NOW()");
+               // $query = DB::query($sql);
+		$result = $sql->execute();
+		
+		return $result;
+        }
 
 }
 ?>
